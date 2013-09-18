a9fb19170be1c170ceb6fcdef818b62b6d64db35
bilou <ga_bilou@hotmail.com>
2013-09-18 23:03:40 +0200

    first basic template finished, nothing fancy though

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, url
-from views import home
+from views import home, ticket_listing
 
 urlpatterns = patterns('',
     url(u'^home$', home, name="home"),
-    url(u'^home/(\w+)$', home, name="home")
+    url(u'^home/(\w+)$', home, name="home"),
+    url(u'^$', ticket_listing, name="listing")
 )
--- ticket/views.py
+++ ticket/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 
+from models import Task
 
 def home(request, name=None):
   if name:
@@ -7,3 +8,9 @@ def home(request, name=None):
   else:
     return HttpResponse('hello world!')
 
+def ticket_listing(request):
+  from django.template import Template, Context
+  tickets = Task.objects.all().order_by('-due_date')
+  template = Template('{% for elem in objects %} <strong>{{ elem.name }}</strong>: <em>{{ elem.due_date }}</em> <br />{% endfor %}')
+  context = Context({'objects': tickets})
+  return HttpResponse(template.render(context))
