5c9f2520e078cab63b5938fa857915452314a7cb
bilou <ga_bilou@hotmail.com>
2013-09-18 23:52:32 +0200

    task detail template added+tasks list view&template with many filters added as well

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, url
-from views import home, ticket_listing
+from views import home, ticket_list, ticket_detail
 
 urlpatterns = patterns('',
     url(u'^home$', home, name="home"),
     url(u'^home/(\w+)$', home, name="home"),
-    url(u'^$', ticket_listing, name="listing")
+    url(u'^list/(\w+)$', ticket_list, name="list"),
+    url(u'^detail/(\d+)$', ticket_detail, name="detail")
 )
--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,5 @@
+from datetime import date, timedelta
+
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
@@ -9,6 +11,19 @@ def home(request, name=None):
   else:
     return HttpResponse('hello world!')
 
-def ticket_listing(request):
-  tickets = Task.objects.all().order_by('-due_date')
+def ticket_list(request, filter):
+  if filter == 'closed':
+    tickets = Task.objects.filter(closed=True)
+  elif filter == 'opened':
+    tickets = Task.objects.filter(closed=False)
+  elif filter == 'week':
+    tickets = Task.objects.filter(
+        due_date__lte=date.today() + timedelta(days=7),
+        due_date__gte=date.today())
+  elif filter == 'all':
+    tickets = Task.objects.all().order_by('-due_date')
   return render_to_response('ticket/list.html', {'objects': tickets})
+
+def ticket_detail(request, id):
+  ticket = Task.objects.get(pk=id)
+  return render_to_response('ticket/detail.html', {'object': ticket})
