4d8e828f57f3ffefeab7d8cc95ee7383935d3735
bilou <ga_bilou@hotmail.com>
2013-09-19 00:26:59 +0200

    list view is more flexible and more maintainable+form added to help users filter easier

--- ticket/urls.py
+++ ticket/urls.py
@@ -4,6 +4,6 @@ from views import home, ticket_list, ticket_detail
 urlpatterns = patterns('',
     url(u'^home$', home, name="home"),
     url(u'^home/(\w+)$', home, name="home"),
-    url(u'^list/(\w+)$', ticket_list, name="list"),
+    url(u'^list/$', ticket_list, name="list"),
     url(u'^detail/(\d+)$', ticket_detail, name="detail")
 )
--- ticket/views.py
+++ ticket/views.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import datetime
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
@@ -11,17 +11,21 @@ def home(request, name=None):
   else:
     return HttpResponse('hello world!')
 
-def ticket_list(request, filter):
-  if filter == 'closed':
-    tickets = Task.objects.filter(closed=True)
-  elif filter == 'opened':
-    tickets = Task.objects.filter(closed=False)
-  elif filter == 'week':
-    tickets = Task.objects.filter(
-        due_date__lte=date.today() + timedelta(days=7),
-        due_date__gte=date.today())
-  elif filter == 'all':
-    tickets = Task.objects.all().order_by('-due_date')
+def ticket_list(request):
+  format = '%Y-%m-%d'
+  tickets = Task.objects.all()
+  if request.method == 'GET':
+    if 'closed' in request.GET:
+      if request.GET['closed'] == 'true':
+        tickets = tickets.filter(closed=True)
+      else:
+        tickets = tickets.filter(closed=False)
+        if 'start' in request.GET and request.GET['start']:
+          tickets = tickets.filter(
+              due_date__gte=datetime.strptime(request.GET['start'], format))
+        if 'end' in request.GET and request.GET['end']:
+          tickets = tickets.filter(
+              due_date__lte=datetime.strptime(request.GET['end'], format))
   return render_to_response('ticket/list.html', {'objects': tickets})
 
 def ticket_detail(request, id):
